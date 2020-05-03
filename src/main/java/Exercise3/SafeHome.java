d794a3abb4386f5acf8fa8ec81cf11f597b0dad1
Ovidiu Cretu <cretuovidiudaniel@yahoo.com>
2020-05-03 19:25:44 +0300

    Done

--- src/main/java/Exercise3/SafeHome.java
+++ src/main/java/Exercise3/SafeHome.java
@@ -5,12 +5,8 @@ public class SafeHome {
     public static void main(String[] args) throws Exception{
         Door door = new Door(DoorStatus.CLOSE);
         DoorLockController dr1 = new DoorLockController(door);
-        ControllerInterface ctrl1;
-       /// dr1.addTenant("123","User1");
-        ///dr1.addTenant("124","b");
-       /// dr1.addTenant("125","a");
-        ///dr1.removeTenant("User1");
-        ///dr1.enterPin("124");
+        ControllerInterface ctrl1=dr1;
+        ///ctrl1.addTenant("1234","User1");
         GUI gui = new GUI();
         dr1.getLogs();
     }
